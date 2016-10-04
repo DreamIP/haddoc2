10b9c987329a128be5872b132b17b8e3609af145
Kamel ABDELOUAHAB <ke.abdelouahab@gmail.com>
2016-10-04 12:00:10 +0200

    [sumElement] : Updated design to in order to be supported in GPStudio

--- ip/hdl/sumElement.vhd
+++ ip/hdl/sumElement.vhd
@@ -5,6 +5,23 @@ library ieee;
 library work;
 	use work.cnn_types.all;
 
+--
+--                    ----------------
+--                   |                |
+-- in_data(0)________|                |______ out_data
+-- in_data(1)________|                |______ out_dv
+-- in_data(2)________|                |______ ou_fv
+--   ...             |                |
+--                   |       SE       |
+-- in_dv(0)__________|                |
+-- in_dv(1)__________|                |
+-- ...               |                |
+-- in_fv(0)__________|                |
+-- in_fv(1)__________|                |
+--                    ----------------
+
+
+
 entity sumElement is
 
     generic(
@@ -13,28 +30,38 @@ entity sumElement is
     );
 
     port(
-        in_data         :   pixel_array (0 to NB_IN_FLOWS - 1);
         clk	            :	in 	std_logic;
         reset_n	        :	in	std_logic;
         enable          :	in	std_logic;
-        out_data        :   out std_logic_vector (PIXEL_SIZE - 1 downto 0)
+
+        in_data         :   in pixel_array (0 to NB_IN_FLOWS - 1);
+        in_dv           :   in std_logic_vector(0 to NB_IN_FLOWS - 1);
+        in_fv           :   in std_logic_vector(0 to NB_IN_FLOWS - 1);
+
+        out_data        :   out std_logic_vector (PIXEL_SIZE - 1 downto 0);
+        out_dv          :   out std_logic;
+        out_fv          :   out std_logic
     );
 end entity;
 --
 architecture bhv of sumElement is
 
-    -- Gestion de l'overflow de manière très approximative ...
     type pixel_array_signed is array (0 to NB_IN_FLOWS - 1) of signed (PIXEL_SIZE -1 downto 0);
     signal	data_s	    :	pixel_array_signed ;
     signal  sum_s       :   signed (PIXEL_SIZE - 1 downto 0);
+    signal  tmp_dv      :   std_logic;
+    signal  tmp_fv      :   std_logic;
 
+    -- Cast Pixels in signed
     begin
     CAST : for i in 0 to (NB_IN_FLOWS - 1) generate
         data_s(i)      <=  signed(in_data(i));
     end generate;
 
+
+
     process(clk)
-    -- Gestion de l'overflow de manière très approximative ...
+    -- TODO : Overflow management
         variable sum    :   signed (PIXEL_SIZE - 1  downto 0);
         begin
             if (reset_n ='0') then
@@ -47,7 +74,7 @@ architecture bhv of sumElement is
                     SUM_LOOP : for i in 0 to (NB_IN_FLOWS - 1) loop
                         sum := sum + data_s(i);
                     end loop;
-
+    -- ReLU Activation Function : 0 threshold
                     if (sum(sum'left) = '1')	then
                         sum := (others => '0');
                     end if;
@@ -56,8 +83,15 @@ architecture bhv of sumElement is
                 end if;
             end if;
         end process;
-        out_data <= std_logic_vector (sum_s);
-        
-        -- Gestion de out_valid : Pour le moment, clone enable
-        out_valid <= enable;
+
+
+    out_data <= std_logic_vector (sum_s);
+
+    --------------------------------------------------------------------------
+    -- DataValid and FlowValid Management :
+    --------------------------------------------------------------------------
+    -- out_dv => '1' when all in in_dvs (plural) are at 1
+    -- Unary operators : Only supported in VHDL-2008
+    out_dv <= and in_dv;
+    out_fv <= and in_fv;
 end bhv;
