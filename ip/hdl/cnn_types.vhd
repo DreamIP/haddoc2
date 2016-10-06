b89bd6cab20866519f413a323c482f83cab058e4
Kamel ABDELOUAHAB <ke.abdelouahab@gmail.com>
2016-10-06 10:05:31 +0200

    [convLayer] Added convLayer : A generic convolution layer design file

--- ip/hdl/cnn_types.vhd
+++ ip/hdl/cnn_types.vhd
@@ -3,5 +3,7 @@ library ieee;
 
 package	cnn_types is
 	constant PIXEL_CONST	:	integer := 8;
+	constant KERNEL_CONST	:	integer := 3;
 	type	 pixel_array   is array ( integer range <> ) of std_logic_vector (PIXEL_CONST-1 downto 0);
+	type	 kernel_array   is array ( integer range <> ) of pixel_array (KERNEL_CONST * KERNEL_CONST-1 downto 0);
 end cnn_types;
