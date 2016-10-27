8010eecb73f7c9d0b5f84976328b7c3519672da1
Kamel ABDELOUAHAB <ke.abdelouahab@gmail.com>
2016-10-27 15:39:17 +0200

    [parser] FC Layer params are now parsed into VHDL constants

--- lib/parseLayer.py
+++ lib/parseLayer.py
@@ -47,7 +47,6 @@ def to_scaleFactor(nbits):
 ######################################################################
 def write_bias_value (layer,name,nbits,target):
     layer_name   = name;
-    kernel_data  = layer[0].data
     bias_data    = layer[1].data
 
     scale_factor = to_scaleFactor(nbits)
@@ -55,7 +54,7 @@ def write_bias_value (layer,name,nbits,target):
 
     target.write("constant ")
     target.write(layer_name)
-    target.write("_BIAS_VALUE  :  pixel_array :=")
+    target.write("_BIAS_VALUE   :  pixel_array :=")
     target.write("   (0 to " + layer_name + "_OUT_SIZE - 1 ) := \n")
     bias_fp      = to_fixedPoint(bias_data,scale_factor)
 
@@ -83,7 +82,7 @@ def write_kernel_value (layer,name,nbits,target):
     target.write(layer_name)
     target.write("_KERNEL_VALUE :  pixel_matrix :=")
     # (0 to CONV1_LAYER_SIZE * CONV1_LAYER_SIZE - 1, 0 to CONV1_KERNEL_SIZE*CONV1_KERNEL_SIZE - 1) :=
-    target.write(" (0 to " + layer_name + "_IN_SIZE * " + layer_name + "_OUTSIZE - 1 ,")
+    target.write(" (0 to " + layer_name + "_IN_SIZE * " + layer_name + "_OUT_SIZE - 1 ,")
     target.write("  0 to " + layer_name + "_KERNEL_SIZE * "+ layer_name + "_KERNEL_SIZE - 1)")
     target.write(" :=\n")
 
@@ -129,6 +128,70 @@ def write_kernel_norm (layer,name,nbits,target):
                 target.write (");\n")
             else:
                 target.write(",")
+
+######################################################################
+def write_fc_kernel_value (previous_layer,layer,name,nbits,target):
+    layer_name   = name;
+    kernel_data  = layer[0].data
+    out_size     = kernel_data.shape[0]
+    in_size      = previous_layer[0].data.shape[0]
+    kernel_size  = int(math.sqrt(kernel_data.shape[1]/in_size))
+    kernel_data  = np.reshape(kernel_data,(out_size,in_size,kernel_size,kernel_size))
+    scale_factor = to_scaleFactor(nbits)
+
+    # constant CONV1_KERNEL_VALUE : pixel_matrix
+    target.write("constant ")
+    target.write(layer_name)
+    target.write("_KERNEL_VALUE :  pixel_matrix :=")
+    # (0 to CONV1_LAYER_SIZE * CONV1_LAYER_SIZE - 1, 0 to CONV1_KERNEL_SIZE*CONV1_KERNEL_SIZE - 1) :=
+    target.write(" (0 to " + layer_name + "_IN_SIZE * " + layer_name + "_OUT_SIZE - 1 ,")
+    target.write("  0 to " + layer_name + "_KERNEL_SIZE * "+ layer_name + "_KERNEL_SIZE - 1)")
+    target.write(" :=\n")
+
+    kernel_fp    = to_fixedPoint(kernel_data,scale_factor)
+
+    target.write(" (")
+    for n in range(out_size):
+        for m in range (in_size):
+            target.write("(")
+            for i in range(kernel_size):
+                for j in range(kernel_size):
+                    kernel_bin = np.binary_repr(kernel_fp[n][m][i][j] , width=nbits)
+                    target.write ("\"" + kernel_bin + "\"")
+                    if ((i == kernel_size - 1) and (j == kernel_size -1) ):
+                        if ((n == out_size - 1) and (m == in_size - 1) ):
+                            target.write (")\n")
+                        else:
+                            target.write("),\n  ")
+                    else:
+                        target.write(",")
+    target.write(" );\n")
+######################################################################
+def write_fc_kernel_norm (previous_layer,layer,name,nbits,target):
+    layer_name   = name;
+    kernel_data  = layer[0].data
+    out_size     = kernel_data.shape[0]
+    in_size      = previous_layer[0].data.shape[0]
+    kernel_size  = int(math.sqrt(kernel_data.shape[1]/in_size))
+    kernel_data  = np.reshape(kernel_data,(out_size,in_size,kernel_size,kernel_size))
+    scale_factor = to_scaleFactor(nbits)
+
+    target.write("constant ")
+    target.write(layer_name)
+    target.write("_KERNEL_NORM  :  pixel_array :=")
+    target.write("   (0 to " + layer_name + "_IN_SIZE * "+ layer_name + "_OUT_SIZE - 1 ) := \n")
+
+    kernel_fp    = to_fixedPoint(kernel_data,scale_factor)
+    target.write(" (")
+    for n in range(out_size):
+        for m in range (in_size):
+            shift_norm = to_shiftNorm(kernel_fp[n][m]);
+            shift_norm_bin = np.binary_repr(shift_norm , width=nbits);
+            target.write ("\"" + shift_norm_bin + "\"")
+            if ((n == out_size - 1) and (m == in_size - 1) ):
+                target.write (");\n")
+            else:
+                target.write(",")
 ######################################################################
 def parse_convLayer (layer,name,nbits,target,image_width):
     layer_name   = name;
@@ -138,30 +201,48 @@ def parse_convLayer (layer,name,nbits,target,image_width):
     kernel_size  = kernel_data.shape[2]
     ## Write layer params ##
     target.write("--" + layer_name + "\n")
+    write_image_width (layer_name,image_width,target);
     write_in_size (layer_name,in_size,target);
     write_out_size (layer_name,out_size,target);
     write_kernel_size (layer_name,kernel_size,target);
-    write_image_width (layer_name,image_width,target);
-    write_kernel_value (layer,name,nbits,target);
     write_bias_value (layer,name,nbits,target);
     write_kernel_norm (layer,name,nbits,target);
-    target.write("------------------------------------------------------------------------------------------------------------------\n")
+    write_kernel_value (layer,name,nbits,target);
+    target.write("----------------------------------------------------------")
+    target.write("--------------------------------------------------------\n")
 ######################################################################
 def parse_poolLayer(blob,name,target,image_width):
     layer_name   = name;
     kernel_size  = 2; #For now only a subsampling factor of 4 is supported
     out_size     = blob.data.shape[1];
     target.write("--" + layer_name + "\n")
+    write_image_width (layer_name,image_width,target);
     write_out_size (layer_name,out_size,target);
     write_kernel_size (layer_name,kernel_size,target);
+    target.write("----------------------------------------------------------")
+    target.write("--------------------------------------------------------\n")
+######################################################################
+def parse_fcLayer(previous_layer,layer,name,nbits,target,image_width):
+    layer_name   = name;
+    kernel_data  = layer[0].data
+    out_size     = kernel_data.shape[0]
+    # Number of in features is the number of out features of previous layers
+    in_size      = previous_layer[0].data.shape[0]
+    # HACK:if everything goes right, we can compute kernel size of FC as follows:
+    kernel_size  = int(math.sqrt(kernel_data.shape[1]/in_size))
+    ## Write layer params ##
+    target.write("--" + layer_name + "\n")
     write_image_width (layer_name,image_width,target);
-    target.write("------------------------------------------------------------------------------------------------------------------\n")
+    write_in_size (layer_name,in_size,target);
+    write_out_size (layer_name,out_size,target);
+    write_kernel_size (layer_name,kernel_size,target);
+    write_bias_value (layer,name,nbits,target);
+    #write_fc_kernel_norm (previous_layer,layer,name,nbits,target);
+    write_fc_kernel_value (previous_layer,layer,name,nbits,target);
+    target.write("----------------------------------------------------------")
+    target.write("--------------------------------------------------------\n")
 ######################################################################
-# def write_kernel_norm (layer_name,value,target):
-#
-#
-######################################################################
-# def write_kernel_value (layer_name,value,target):
+
 
 if __name__ == "__main__":
 
@@ -176,6 +257,7 @@ if __name__ == "__main__":
     conv1 = cnn.params['conv1']
     conv2 = cnn.params['conv2']
     conv3 = cnn.params['conv3']
+    fc    = cnn.params['fc']
     s1    = cnn.blobs['s1']
     s2    = cnn.blobs['s2']
 
@@ -185,3 +267,4 @@ if __name__ == "__main__":
         parse_convLayer(conv2,'CONV2',8,f,158)
         parse_poolLayer(s2,'POOL2',f,154)
         parse_convLayer(conv3,'CONV3',8,f,77)
+        parse_fcLayer  (conv3,fc,'FC',8,f,73)
