d076b729d4800b815f16b137773d55f886311256
Kamel ABDELOUAHAB <ke.abdelouahab@gmail.com>
2017-03-07 10:55:55 +0100

    Updated example

--- example/make_example.sh
+++ example/make_example.sh
@@ -1,5 +1,4 @@
 echo "make_example.sh"
 echo "This script generates full VHDL code of the the CNN described in ./example/caffe"
-../bin/haddoc2 ./caffe/deploy.prototxt ./caffe/network.caffemodel ./hdl_generated
-python ../lib/quartus.py
-echo "Generated Quartus project"
+../bin/haddoc2  ./caffe/lenet.prototxt ./caffe/lenet.caffemodel ./hdl_generated
+python ../lib/quartus.py ./
