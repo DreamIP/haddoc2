923152cd0bc7bb82de796b8d54321a487ee5e32e
KamelAbdelouahab <ke.abdelouahab@gmail.com>
2017-04-10 14:10:47 +0200

    Updated Quartus project generator

--- lib/quartus.py
+++ lib/quartus.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-def gen_qsf (qsfFilename):
+def gen_qsf (qsfFilename,haddoc_lib_dir):
      with open (qsfFilename,'w') as f:
          # Device
          f.write("set_global_assignment -name FAMILY \"Stratix V\"\n");
@@ -16,30 +16,29 @@ def gen_qsf (qsfFilename):
          f.write("set_global_assignment -name POWER_PRESET_COOLING_SOLUTION \"23 MM HEAT SINK WITH 200 LFPM AIRFLOW\"\n");
          f.write("set_global_assignment -name POWER_BOARD_THERMAL_MODEL \"NONE (CONSERVATIVE)\"\n");
 
-         # LE Based arithmetic
+         # Logic Elements Based arithmetic
          f.write("set_global_assignment -name DSP_BLOCK_BALANCING \"LOGIC ELEMENTS\"\n");
          f.write("set_global_assignment -name AUTO_DSP_RECOGNITION OFF\n");
 
          # Haddoc lib
-         # TODO : autogenerate
          f.write("set_global_assignment -name VHDL_FILE cnn_process.vhd\n");
          f.write("set_global_assignment -name VHDL_FILE params.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/cnn_types.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/convElement.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/to_signedPixel.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/maxPool.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/fcLayer.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/display_mux.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/poolLayer.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/taps.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/cnn_types.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/sumElement_single.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/convLayer.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/poolV.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/firstLayer.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/sumElement.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/neighExtractor.vhd\n");
-         f.write("set_global_assignment -name VHDL_FILE /home/kamel/dev/haddoc2/lib/hdl/poolH.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/cnn_types.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/convElement.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/to_signedPixel.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/maxPool.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/fcLayer.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/display_mux.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/poolLayer.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/taps.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/cnn_types.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/sumElement_single.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/convLayer.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/poolV.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/firstLayer.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/sumElement.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/neighExtractor.vhd\n");
+         f.write("set_global_assignment -name VHDL_FILE " + haddoc_lib_dir + "/poolH.vhd\n");
          f.write("set_instance_assignment -name PARTITION_HIERARCHY root_partition -to | -section_id Top\n");
          f.close();
 
@@ -49,17 +48,22 @@ def gen_qpf(qpfFilename):
         f.write("PROJECT_REVISION = \"cnn_process\"\n");
         f.close();
 
-def main(qsfFilename,qpfFilename):
-    gen_qsf (qsfFilename);
+def main(qsfFilename,qpfFilename,haddoc_lib_dir):
+    gen_qsf (qsfFilename,haddoc_lib_dir);
     gen_qpf (qpfFilename);
 
 
 if __name__ == '__main__':
     if (len(sys.argv) == 2):
+        cwd       = os.getcwd()
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        haddoc_lib_dir = file_path + '/hdl'
+        print "Haddoc2 lib in " + haddoc_lib_dir
         targetDir = sys.argv[1];
         qsfFilename = targetDir + "/cnn_process.qsf";
         qpfFilename = targetDir + "/cnn_process.qpf";
-        main(qsfFilename,qpfFilename);
+
+        main(qsfFilename,qpfFilename,haddoc_lib_dir);
         print 'Succefully generated quartus project'
     else:
         print 'Not enought arguments'
