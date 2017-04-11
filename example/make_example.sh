06ec980359ec3d71f5b05cbef321a27fd12dfa50
KamelAbdelouahab <ke.abdelouahab@gmail.com>
2017-04-11 09:56:12 +0200

    Removed deprecated make_example script, replaced with Makefile in some previous commit

--- example/make_example.sh
+++
@@ -1,4 +0,0 @@
-echo "make_example.sh"
-echo "This script generates full VHDL code of the the CNN described in ./example/caffe"
-../bin/haddoc2  ./caffe/lenet.prototxt ./caffe/lenet.caffemodel ./hdl_generated
-python ../lib/quartus.py ./
