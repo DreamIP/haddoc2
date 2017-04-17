1b373a3ca372646a598cc4e305c275bddda95404
KamelAbdelouahab <ke.abdelouahab@gmail.com>
2017-04-17 13:30:19 +0200

    Updated layer type recognition: from syntax based tocaffe\'s layer.type method

--- lib/python/parseNet.py
+++ lib/python/parseNet.py
@@ -23,26 +23,49 @@ def main(vhdFile, protoFile, modelFile,pixel_width):
     blobs = cnn.blobs
     previousLayer = cnn.params['conv1'];
     for b in blobs.keys():
-        if 'label' in b or 'cla' in b:
+        if 'label' in b or 'cla' in b or 'TanH' in b:
             del blobs[b]
     with open (vhdFile,'w') as f:
+
+        layer_id = 0;
         write_fileHead(f)
         for b in blobs.keys():
-            if 'data' in b:
+            if (cnn.layers[layer_id].type == 'Input'):
                 image_width = cnn.blobs[b].data.shape[2]
-            if 'conv' in b:
+
+            if (cnn.layers[layer_id].type == 'Convolution'):
+                print b
+                print cnn.layers[layer_id].type
+                print '\n'
                 layer = cnn.params[b];
                 parse_convLayer(previous_layer = previousLayer, layer=layer,name=b.upper(),nbits=pixel_width,target=f,image_width=image_width);
                 image_width = cnn.blobs[b].data.shape[2]
                 previousLayer = cnn.params[b];
-            if 'pool'in b:
+                if (cnn.layers[layer_id+1].type == 'TanH'): # Assuming non-linearity bloc after each conv layer
+                    layer_id +=1;
+
+            if (cnn.layers[layer_id].type == 'Pooling'):
                 blob  = cnn.blobs[b];
+                print b
+                print cnn.layers[layer_id].type
+                print '\n'
                 parse_poolLayer(blob=blob,name=b.upper(),target=f,image_width=image_width);
                 image_width = cnn.blobs[b].data.shape[2]
-            if 'fc' in b:
+
+
+            if (cnn.layers[layer_id].type == 'InnerProduct'):
                 layer = cnn.params[b];
                 parse_fcLayer(previous_layer=previousLayer,layer=layer,name=b.upper(),nbits=pixel_width,target=f,image_width=image_width);
                 image_width = cnn.blobs[b].data.shape[2]
+                layer_id +=1;
+
+            # if (cnn.layers[layer_id].type == 'TanH'):
+            #     layer_id +=1;
+            #     print b
+            #     print cnn.layers[layer_id].type
+            #     print '\n'
+
+            layer_id +=1;
         write_fileEnd(f)
         os.environ["GLOG_minloglevel"] = "0"
 
